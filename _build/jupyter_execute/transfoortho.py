758d75997be674fdd2014113cae2d336d3407274
barra <vincent.barra@uca.fr>
2022-07-12 17:12:07 +0200

    changes

--- _build/jupyter_execute/transfoortho.py
+++ _build/jupyter_execute/transfoortho.py
@@ -153,6 +153,15 @@ Q = Rational(1, 3) * Matrix([[1, -2], [2, -1], [2, 2]])
 Q
 
 
+# Les colonnes de $Q$ forment une base de $\mathbb{R}^2$ (dans $\mathbb{R}^3$). Pour obtenir une matrice orthogonale, il faut compléter par un vecteur normal au plan défini par ces deux colonnes, et unitaire.
+
+# In[6]:
+
+
+Q = Rational(1, 3) * Matrix([[1, -2, 2], [2, -1, -2], [2, 2, 1]])
+Q
+
+
 # Appliqué au problème des moindres carrés, le système aux équations normales s'écrit donc 
 # 
 # $\begin{eqnarray*}
