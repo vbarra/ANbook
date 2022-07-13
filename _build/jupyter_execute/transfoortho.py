b386b431289ea5d173696736d9ff56270d042c98
barra <vincent.barra@uca.fr>
2022-07-13 10:23:36 +0200

    changes

--- _build/jupyter_execute/transfoortho.py
+++ _build/jupyter_execute/transfoortho.py
@@ -376,19 +376,15 @@ Q
 
 
 def householder(A):
-    """Performs a Householder Reflections based QR Decomposition of the                                               
-    matrix A. The function returns Q, an orthogonal matrix and R, an                                                  
-    upper triangular matrix such that A = QR."""
     n = len(A)
 
-    # Set R equal to A, and create Q as a zero matrix of the same size
     R = A
-    Q = [[0.0] * n for i in xrange(n)]
+    Q = np.zeros(n,n)
 
     # The Householder procedure
-    for k in range(n-1):  # We don't perform the procedure on a 1x1 matrix, so we reduce the index by 1
+    for k in range(n-1):  
         # Create identity matrix of same size as A                                                                    
-        I = [[float(i == j) for i in xrange(n)] for j in xrange(n)]
+        I = np.eye(n,n)
 
         # Create the vectors x, e and the scalar alpha
         # Python does not have a sgn function, so we use cmp instead
@@ -401,24 +397,20 @@ def householder(A):
         norm_u = norm(u)
         v = map(lambda p: p/norm_u, u)
 
-        # Create the Q minor matrix
-        Q_min = [ [float(i==j) - 2.0 * v[i] * v[j] for i in xrange(n-k)] for j in xrange(n-k) ]
+        # Sous matrice Q
+        Q_min = [ [float(i==j) - 2.0 * v[i] * v[j] for i in range(n-k)] for j in range(n-k) ]
 
-        # "Pad out" the Q minor matrix with elements from the identity
-        Q_t = [[ Q_i(Q_min,i,j,k) for i in xrange(n)] for j in xrange(n)]
+        # Intégration à Q 
+        Q_t = [[ Q_i(Q_min,i,j,k) for i in range(n)] for j in range(n)]
 
-        # If this is the first run through, right multiply by A,
-        # else right multiply by Q
         if k == 0:
             Q = Q_t
-            R = mult_matrix(Q_t,A)
+            R = Q_t@A
         else:
-            Q = mult_matrix(Q_t,Q)
-            R = mult_matrix(Q_t,R)
+            Q = Q_t@Q
+            R = Q_t@R
 
-    # Since Q is defined as the product of transposes of Q_t,
-    # we need to take the transpose upon returning it
-    return trans_matrix(Q), R
+    return Q.transpose(), R
 
 A = [[12, -51, 4], [6, 167, -68], [-4, 24, -41]]
 Q, R = householder(A)
