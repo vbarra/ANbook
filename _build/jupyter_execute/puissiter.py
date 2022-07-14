20ac588f959131741f16b2a9510f4c3aa1993d3a
barra <vincent.barra@uca.fr>
2022-07-14 08:49:25 +0200

    changes

--- _build/jupyter_execute/puissiter.py
+++ _build/jupyter_execute/puissiter.py
@@ -77,7 +77,7 @@ def puissiter(A,v0,niter=5):
         ll.append(lamda)
     return ll, vv
 
-A = np.array([[2.,1,1],[1,3,-5],[7,1,4]])
+A = np.array([[2.,1,-1],[1,3,1],[-1,1,4]])
 lam =(np.linalg.eigvals(A)[0])
 print("La plus grande valeur propre de A est ",lam)
 ll, vv = puissiter(A,np.ones(3))
