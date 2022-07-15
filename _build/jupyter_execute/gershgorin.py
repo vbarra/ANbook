930d2c41f2a0f317bb621e0f000d5e98090d3ad8
barra <vincent.barra@uca.fr>
2022-07-15 09:25:11 +0200

    changes

--- _build/jupyter_execute/gershgorin.py
+++ _build/jupyter_execute/gershgorin.py
@@ -5,6 +5,10 @@
 # 
 # Avant d'aborder quelques algorithmes de calcul des valeurs propres d'une matrice, donnons une alternative pratique à ces algorithmes . Le théorème suivant  permet de localiser les valeurs propres dans des disques, dits disques de Gershgorin, du plan complexe.
 # 
+# ```{margin} 
+# ![](./images/gershgorinsemyon.png)
+# ```
+# 
 # ```{prf:theorem} Théorème de Gershgorin
 # Si on représente une matrice $A$ (ou toute matrice semblable à $A$) sous la forme $A=diag\{d_1\cdots d_n\}+F$, où $F$ est une matrice de diagonale nulle, alors le spectre de $A$ est contenu dans l'union des disques $D_i,1\leq i\leq n$ du plan complexe, tels que 
 # 
@@ -14,9 +18,7 @@
 # ```{index} Gershgorin;théorème
 # ```
 # 
-# ```{margin} 
-# ![](./images/gershgorinsemyon.png)
-# ```
+# 
 # 
 # 
 # 
