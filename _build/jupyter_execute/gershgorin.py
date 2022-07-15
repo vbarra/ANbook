244bf8d55821e02937b816a104e82915b8c7600a
barra <vincent.barra@uca.fr>
2022-07-15 08:40:29 +0200

    changes

--- _build/jupyter_execute/gershgorin.py
+++ _build/jupyter_execute/gershgorin.py
@@ -39,3 +39,31 @@
 # D_3&=\left \{ z\in \mathbb{C}, |z-3|\leq 0.2\right\}
 # \end{align*}$
 # ```
+
+# In[1]:
+
+
+N = 5 # dimension of our square matrix
+
+D = np.diag([0, 3 + 1j, 4 + 1j, 1 + 5j, 9 + 2j])
+M = np.random.rand(N, N) + D
+
+R = np.zeros(N) # disk radii
+for i in range(N):
+    R[i] = sum(abs(M[i,:])) - abs(M[i,i])
+
+eigenvalues = np.linalg.eigvals(M)
+
+# Plotting code
+fig, ax = plt.subplots()
+for k in range(N):
+    x, y = M[k,k].real, M[k,k].imag
+    ax.add_artist( plt.Circle((x, y), R[k], alpha=0.5) )
+    plt.plot(eigenvalues[k].real, eigenvalues[k].imag, 'k+')
+
+ax.axis([-4, 12.5, -4, 9])
+ax.set_aspect(1)    
+plt.xlabel("$x$")
+plt.ylabel("$y$")
+plt.title("Gershgorin disks and eigenvalues $x + iy$")
+
