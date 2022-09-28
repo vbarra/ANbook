463e23e369957b185b36ebbae51f6e8026d969b7
barra <vincent.barra@uca.fr>
2022-09-28 12:05:13 +0200

    changes

--- _build/jupyter_execute/lu.py
+++ _build/jupyter_execute/lu.py
@@ -339,5 +339,5 @@ A, L * U
 # \det({\bf A})=(-1)^p\prod_{i=1}^nu_{ii}
 # $
 # 
-# où les $u_{ii}$ ($i\in[\![1, n]\!]$) sont les pivots et $p$ le nombre de permutations
+# où les $u_{ii}, i\in[\![1, n]\!]$ sont les pivots et $p$ le nombre de permutations
 # effectuées au cours de la factorisation.
